cf81d8eeac21720a27fe52e231248e052dfadf8f
Nicholas Jamieson <nicholas@cartant.com>
2018-05-06 17:50:39 +1000

    refactor(unused): Remove _names set.

--- source/rules/noUnusedDeclarationRule.ts
+++ source/rules/noUnusedDeclarationRule.ts
@@ -31,7 +31,6 @@ export class Rule extends Lint.Rules.TypedRule {
 export class Walker extends Lint.ProgramAwareRuleWalker {
 
     private _identifiers = new Map<ts.Node, boolean>();
-    private _names = new Set<string>();
 
     protected onSourceFileEnd(): void {
 
@@ -47,19 +46,17 @@ export class Walker extends Lint.ProgramAwareRuleWalker {
 
         const { name } = node;
         if (name && !tsutils.hasModifier(node.modifiers, ts.SyntaxKind.ExportKeyword)) {
-            const { _identifiers, _names } = this;
+            const { _identifiers } = this;
             _identifiers.set(name, false);
-            _names.add(name.getText());
         }
         super.visitFunctionDeclaration(node);
     }
 
     protected visitIdentifier(node: ts.Identifier): void {
 
-        const { _identifiers, _names } = this;
+        const { _identifiers } = this;
         const isDeclaration = _identifiers.has(node);
-        const isExpected = _names.has(node.getText());
-        if (!isDeclaration && isExpected) {
+        if (!isDeclaration) {
 
             const typeChecker = this.getTypeChecker();
             const symbol = typeChecker.getSymbolAtLocation(node);
@@ -76,24 +73,22 @@ export class Walker extends Lint.ProgramAwareRuleWalker {
 
     protected visitNamedImports(node: ts.NamedImports): void {
 
-        const { _identifiers, _names } = this;
+        const { _identifiers } = this;
         node.elements.forEach(element => {
             const { name, propertyName } = element;
             if (propertyName) {
                 _identifiers.set(propertyName, true);
             }
             _identifiers.set(name, false);
-            _names.add(name.getText());
         });
         super.visitNamedImports(node);
     }
 
     protected visitNamespaceImport(node: ts.NamespaceImport): void {
 
-        const { _identifiers, _names } = this;
+        const { _identifiers } = this;
         const { name } = node;
         _identifiers.set(name, false);
-        _names.add(name.getText());
         super.visitNamespaceImport(node);
     }
 
@@ -109,11 +104,10 @@ export class Walker extends Lint.ProgramAwareRuleWalker {
     protected visitVariableStatement(node: ts.VariableStatement): void {
 
         if (!tsutils.hasModifier(node.modifiers, ts.SyntaxKind.ExportKeyword)) {
-            const { _identifiers, _names } = this;
+            const { _identifiers } = this;
             tsutils.forEachDeclaredVariable(node.declarationList, declaration => {
                 const { name } = declaration;
                 _identifiers.set(name, false);
-                _names.add(name.getText());
             });
         }
         super.visitVariableStatement(node);
