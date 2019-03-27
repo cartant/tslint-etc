06f2f59f952c29913447f0b277c6b52943f948d1
Nicholas Jamieson <nicholas@cartant.com>
2019-03-27 12:09:06 +1000

    feat(enum): Add no-enum and no-const-enum rules.

---
+++ source/rules/noEnumRule.ts
@@ -0,0 +1,41 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/tslint-etc
+ */
+
+import * as Lint from "tslint";
+import * as ts from "typescript";
+import { tsquery } from "@phenomnomnominal/tsquery";
+
+export class Rule extends Lint.Rules.TypedRule {
+
+    public static metadata: Lint.IRuleMetadata = {
+        description: "Disallows the use of enums.",
+        options: null,
+        optionsDescription: "Not configurable.",
+        requiresTypeInfo: false,
+        ruleName: "no-enum",
+        type: "functionality",
+        typescriptOnly: true
+    };
+
+    public static FAILURE_STRING = "enum is forbidden";
+
+    public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
+        const failures: Lint.RuleFailure[] = [];
+        const enumDeclarations = tsquery(
+            sourceFile,
+            "EnumDeclaration"
+        );
+        enumDeclarations.forEach((node: ts.Node) => {
+            failures.push(new Lint.RuleFailure(
+                sourceFile,
+                node.getStart(),
+                node.getStart() + node.getWidth(),
+                Rule.FAILURE_STRING,
+                this.ruleName
+            ));
+        });
+        return failures;
+    }
+}
