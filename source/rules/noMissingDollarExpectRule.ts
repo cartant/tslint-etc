074a373c9d2c1d1eed0b64a503f4d5a1a2cf15ec
Nicholas Jamieson <nicholas@cartant.com>
2019-11-03 22:31:02 +1000

    refactor: Reimplement no-missing-dollar.
    Using the no-dtslint-typo rule.

--- source/rules/noMissingDollarExpectRule.ts
+++ source/rules/noMissingDollarExpectRule.ts
@@ -4,9 +4,9 @@
  */
 
 import * as Lint from "tslint";
-import * as ts from "typescript";
+import { Rule as NoDtslintTypoRule } from "./noDtslintTypoRule";
 
-export class Rule extends Lint.Rules.AbstractRule {
+export class Rule extends NoDtslintTypoRule {
   public static metadata: Lint.IRuleMetadata = {
     description:
       "Disallows dtslint $ExpectType and $ExpectError expectations if the $ is missing.",
@@ -17,28 +17,4 @@ export class Rule extends Lint.Rules.AbstractRule {
     type: "functionality",
     typescriptOnly: false
   };
-
-  public static FAILURE_STRING = "Missing $";
-
-  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const failures: Lint.RuleFailure[] = [];
-    const text = sourceFile.getText();
-    const regExp = /\/\/\s+Expect(Type|Error)/g;
-    let result: RegExpExecArray;
-
-    /*tslint:disable:no-conditional-assignment*/
-    while ((result = regExp.exec(text))) {
-      failures.push(
-        new Lint.RuleFailure(
-          sourceFile,
-          result.index,
-          result.index + result[0].length,
-          Rule.FAILURE_STRING,
-          this.ruleName
-        )
-      );
-    }
-    /*tslint:enable:no-conditional-assignment*/
-    return failures;
-  }
 }
